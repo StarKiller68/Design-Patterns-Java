ac00ea34f2cc82658974e226056d6a48bc251902
hbreck <hbreckenridge@yahoo.com>
2023-10-05 14:44:39 -0400

    Added source code for Factory pattern in Factory package

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Builder.*;
+import Factory.*;
 import Singleton.*;
 public class Main {
     public static void main(String[] args) {
@@ -60,5 +61,10 @@ public class Main {
                 .build();
 
         System.out.println(user3);*/
+
+        Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+        Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+        System.out.println("Factory PC Config::"+pc);
+        System.out.println("Factory Server Config::"+server);
     }
 }
